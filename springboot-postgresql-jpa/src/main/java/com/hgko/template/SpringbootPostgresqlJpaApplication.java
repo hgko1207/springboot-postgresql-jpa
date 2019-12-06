08eac7bc03aa90a21227f9777b476371e550263f
hgko <hgko@inspace.re.kr>
2019-12-06 15:07:34 +0900

    Template 설정

---
+++ springboot-postgresql-jpa/src/main/java/com/hgko/template/SpringbootPostgresqlJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hgko.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresqlJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresqlJpaApplication.class, args);
+	}
+
+}
